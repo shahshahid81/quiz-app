12362b46cac3377a0d85694ccc83633dff4cd4e1
Shahid Shah <shahshahid2123@gmail.com>
2022-04-24 19:41:59 +0530

    feat: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(QuizApp());
+
+class QuizApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Text('Quiz App', style: TextStyle(color: Colors.red)));
+  }
+}
